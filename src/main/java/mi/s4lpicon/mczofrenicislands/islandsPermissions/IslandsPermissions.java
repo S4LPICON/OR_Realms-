8e7060d7d608c13182986638ac7eaee64c5b112b
Jeferson Pinzón <S4LPICONG4MER3993@GMAIL.COM>
2024-08-13 14:54:25 -0500

    se agrego metodo para quitar una parte del string del nombre del mundo del jugador

--- src/main/java/mi/s4lpicon/mczofrenicislands/islandsPermissions/IslandsPermissions.java
+++ src/main/java/mi/s4lpicon/mczofrenicislands/islandsPermissions/IslandsPermissions.java
@@ -1,4 +1,18 @@
 package mi.s4lpicon.mczofrenicislands.islandsPermissions;
 
 public class IslandsPermissions {
+
+    public static String getStringAfterSlash(String input) {
+        // Dividir el string en partes utilizando el carácter '/'
+        String[] parts = input.split("/");
+
+        // Verificar si hay al menos dos partes
+        if (parts.length > 1) {
+            // Retornar la parte después del '/'
+            return parts[1];
+        } else {
+            // Si no hay '/' en el string, retornar el string original o manejar el caso como desees
+            return input;
+        }
+    }
 }
