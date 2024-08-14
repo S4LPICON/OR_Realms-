a5afea7b1593d2dc86bc8db5dd3f26712d2c2c3e
Jeferson Pinzón <S4LPICONG4MER3993@GMAIL.COM>
2024-08-14 14:09:05 -0500

    tabcompleter update

--- src/main/java/mi/s4lpicon/mczofrenicislands/commandsManager/CommandsTabCompleter.java
+++ src/main/java/mi/s4lpicon/mczofrenicislands/commandsManager/CommandsTabCompleter.java
@@ -1,6 +1,7 @@
 package mi.s4lpicon.mczofrenicislands.commandsManager;
 
 
+import mi.s4lpicon.mczofrenicislands.invitationManager.InvitationManager;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -17,28 +18,40 @@ public class CommandsTabCompleter implements TabCompleter {
     public List<String> onTabComplete(@NotNull CommandSender sender, Command command, @NotNull String alias, String[] args) {
         List<String> completions = new ArrayList<>();
 
+
         // Verify that the command is /islands
         if (command.getName().equalsIgnoreCase("island")) {
-            if (args.length == 1) {
-                // Complete first level subcommands
-                completions.add("create");
-                completions.add("tp");
-                completions.add("ban");
-                completions.add("unban");
-                completions.add("setspawn");
-                completions.add("invite");
-                completions.add("remove");
-            } else if (args.length == 2 && args[0].equalsIgnoreCase("tp")
-                    || args.length == 2 && args[0].equalsIgnoreCase("ban")
-                    || args.length == 2 && args[0].equalsIgnoreCase("unban")
-                    || args.length == 2 && args[0].equalsIgnoreCase("invite")
-                    || args.length == 2 && args[0].equalsIgnoreCase("remove")) {
-                List<String> playerNames = new ArrayList<>();
-                for (Player player : Bukkit.getOnlinePlayers()) {
-                    playerNames.add(player.getName());
+            if (sender instanceof Player) {
+                Player player = (Player) sender;
+                if (args.length == 1) {
+                    // Complete first level subcommands
+                    completions.add("create");
+                    completions.add("tp");
+                    completions.add("ban");
+                    completions.add("unban");
+                    completions.add("setspawn");
+                    completions.add("invite");
+                    completions.add("remove");
+                    completions.add("join");
+                    completions.add("leave");
+                } else if (args.length == 2 && args[0].equalsIgnoreCase("tp")
+                        || args.length == 2 && args[0].equalsIgnoreCase("ban")
+                        || args.length == 2 && args[0].equalsIgnoreCase("unban")
+                        || args.length == 2 && args[0].equalsIgnoreCase("invite")
+                        || args.length == 2 && args[0].equalsIgnoreCase("remove")
+                        || args.length == 2 && args[0].equalsIgnoreCase("leave")) {
+                    List<String> playerNames = new ArrayList<>();
+                    for (Player playera : Bukkit.getOnlinePlayers()) {
+                        if (!playera.getName().equals(player.getName())) {
+                            playerNames.add(playera.getName());
+                        }
+                    }
+                    return playerNames; // Retorna la lista de nombres de jugadores
+                } else if (args.length == 2 && args[0].equalsIgnoreCase("join")) {
+                    return InvitationManager.getInvitationsOfPlayer(player);
                 }
-                return playerNames; // Retorna la lista de nombres de jugadores
             }
+
         }
         return completions;
     }
