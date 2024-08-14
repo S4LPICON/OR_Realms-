19d3aaa65f869d0a9cf689b07536267a28566535
Jeferson Pinzón <S4LPICONG4MER3993@GMAIL.COM>
2024-08-14 11:20:50 -0500

    los jugadores pueden comer en las islas

--- src/main/java/mi/s4lpicon/mczofrenicislands/islandsPermissions/listeners/IslandsEventListener.java
+++ src/main/java/mi/s4lpicon/mczofrenicislands/islandsPermissions/listeners/IslandsEventListener.java
@@ -1,6 +1,8 @@
-package mi.s4lpicon.mczofrenicislands.islandsPermissions.listeners;// Importa los paquetes necesarios
+package mi.s4lpicon.mczofrenicislands.islandsPermissions.listeners;
 
+// Importa los paquetes necesarios
 import mi.s4lpicon.mczofrenicislands.islandsPermissions.IslandsPermissions;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -8,6 +10,8 @@ import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.event.player.*;
+import org.bukkit.event.player.PlayerItemConsumeEvent; // Importa el evento de consumir ítem
+import org.bukkit.inventory.ItemStack;
 
 public class IslandsEventListener implements Listener {
 
@@ -28,7 +32,7 @@ public class IslandsEventListener implements Listener {
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
         if (IslandsPermissions.blockActionPlayer(event.getPlayer())) {
-            event.setCancelled(true); // Cancela el evento, evitando que el jugador interactúe con bloques
+            //event.setCancelled(true); // Cancela el evento, evitando que el jugador interactúe con bloques
         }
     }
 
@@ -60,7 +64,18 @@ public class IslandsEventListener implements Listener {
         }
     }
 
+    @EventHandler
+    public void onPlayerItemConsume(PlayerItemConsumeEvent event) {
+        Player player = event.getPlayer();
+        ItemStack consumedItem = event.getItem();
 
-
-
+        // Verifica si el ítem consumido es una Notch Apple (manzana dorada encantada)
+        if (consumedItem.getType() == Material.ENCHANTED_GOLDEN_APPLE) {
+            // Bloquea el consumo de la Notch Apple
+            if (IslandsPermissions.blockActionPlayer(player)) {
+                event.setCancelled(true); // Cancela el evento, evitando que el jugador consuma la Notch Apple
+                player.sendMessage("¡No puedes consumir Notch Apples en esta isla!"); // Mensaje opcional
+            }
+        }
+    }
 }
